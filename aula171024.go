13c717644bf438a96f6fcc132f82d886b4c6f76c
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2024-10-19 21:12:43 -0300

    Resolvendo exercícios

---
+++ aula171024.go
@@ -0,0 +1,31 @@
+package main
+//import "fmt"
+
+/*5) Faça um algoritmo que leia um número e mostre uma das mensagens: é múltiplo de 3 ou
+não é múltiplo de 3.
+
+func main171024() {
+	var num int
+	fmt.Print("Digite um número para saber se ele é ou não multiplo de 3: ")
+	fmt.Scan(&num)
+
+	if num % 3 == 0 {
+		fmt.Println("Este número é multiplo de 3!")
+	}else{
+		fmt.Println("Este número não é multiplo de 3!")}}*/
+
+/*6) Faça um algoritmo que leia um número e mostre se ele é ou não divisível por 3 e por 7.
+
+func main171024(){
+	var num int
+	fmt.Print("Digite um número para saber se ele é ou não divisível por 3 e por 7: ")
+	fmt.Scan(&num)
+
+	if num % 3 == 0 && num % 7 == 0 {
+		fmt.Println("Este número é divisível por 3 e por 7!")
+	}else if num % 3 == 0{
+		fmt.Println("Este número é divisível por 3 e não por 7!")
+	}else if num % 7 == 0{
+		fmt.Println("Este número é divisível por 7 e não por 3!")
+	}else{
+		fmt.Println("Este número não é divisível por 3 ou por 7!")}}*/
