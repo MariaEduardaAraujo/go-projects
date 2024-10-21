c3ac241b5922906f905e2a637ffb349a95e2b8b7
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2024-10-20 21:11:29 -0300

    Resolvendo exercícios de Go

--- aula191024.go
+++ aula191024.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+//import ("fmt","math")
 
-/*5) Apresente o quadrado de cada um dos números pares entre 1 e 1000, inclusive.*/
+/*5) Apresente o quadrado de cada um dos números pares entre 1 e 1000, inclusive.
 
 func aula191024(){
-	fmt.Println("Oi")
-}
+	for i := 0; i <= 1000; i++ {
+		if i % 2 == 0 {
+			quadrado := math.Pow(float64(i),2)
+			fmt.Printf("O quadrado de %d é: %.1f\n", i, quadrado)}}}*/
 
 /*6) Apresente todos os números divisíveis por 5 que sejam maiores do que 0 e menores ou
-iguais a 200.*/
+iguais a 200.
+
+func aula191024()  {
+    fmt.Println("Os números divisíveis por 5, entre 1 e 200 são: ")
+	for i := 1; i <= 200; i++ {
+		if i % 5 == 0 {
+			fmt.Println(i)}}}*/
