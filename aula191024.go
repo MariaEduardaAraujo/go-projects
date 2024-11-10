097d42e99126abadea08c16b7441ce1bd066fc44
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2024-11-10 13:19:52 -0300

    Resolvendo exercícios

--- aula191024.go
+++ aula191024.go
@@ -1,8 +1,94 @@
 package main
 
-//import ("fmt","math")
+import (
+	"fmt"
+	"math"
+)
 
 //Lista 05
+
+/*1) Leia 5 valores para uma variável A. A seguir mostre quantos valores digitados foram pares,
+quantos valores digitados foram ímpares, quantos foram positivos e quantos foram negativos.
+
+func main(){
+	var A[] int
+	var num int
+	var par, impar, maior, menor int
+
+	fmt.Printf("Digite 5 números: \n")
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Número %d: ", i+1)
+		fmt.Scan(&num)
+		A = append(A,num)
+	}
+
+	for j := 0; j < len(A); j++ {
+		if A[j] % 2 == 0 {
+			par += 1
+		}else{
+			impar += 1
+		}
+
+		if A[j] > 0 {
+			maior += 1
+		}else{
+			menor += 1
+		}
+	}
+
+    fmt.Printf("\n")
+	fmt.Printf("Par: %d\n", par)
+	fmt.Printf("Ímpar: %d\n", impar)
+	fmt.Printf("Maior que 0: %d\n", maior)
+	fmt.Printf("Menor que 0: %d\n", menor)
+}*/
+
+/*2) Calcule e mostre a soma dos números pares entre 1 e 100, inclusive.
+
+func aula191024(){
+	var soma int
+
+	for i := 1; i <= 100; i++ {
+		if i % 2 == 0 {
+			soma = soma + i
+		}
+	}
+
+	fmt.Printf("A soma dos números pares de 1 até 100 é: %d", soma)
+}*/
+
+/*3) Calcule e mostre a média dos números pares entre 1 e 100, inclusive.
+
+func aula191024(){
+	var soma, media, c int
+
+	for i := 1; i <= 100; i++ {
+		if i % 2 == 0 {
+			soma = soma + i
+			c+=1
+		}
+	}
+
+	media = soma/c
+
+	fmt.Printf("A média dos números pares é: %d", media)
+}*/
+
+/*4) Leia 2 valores: X e Y. A seguir, calcule e mostre a soma dos números impares entre eles*/
+
+func aula191024(){
+	var num1, num2 int
+
+	fmt.Printf("Digite dois números: \n")
+	fmt.Printf("Número 1: ")
+	fmt.Scan(&num1)
+	fmt.Printf("Número 2: ")
+	fmt.Scan(&num2)
+
+	for i := 0; i < count; i++ {
+		
+	}
+}
 
 /*5) Apresente o quadrado de cada um dos números pares entre 1 e 1000, inclusive.
 
